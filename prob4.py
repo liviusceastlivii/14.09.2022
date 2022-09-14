1bd1ce64558fab5fc95e0f09b2d15f8583d5bfcd
liviusceastlivii <70839080+liviusceastlivii@users.noreply.github.com>
2022-09-14 22:33:57 +0300

    Update and rename pprob1.py to prob4.py

--- prob4.py
+++ prob4.py
@@ -2,7 +2,7 @@ def creare_lista():
     n=int(input('Nr. de elemente=?'))
     lista_locala=[]
     for i in range(n):
-        elem=input('Elementul'+'str(i)+este:')
+        elem=int(input('Elementul'+'str(i)+este:'))
         lista_locala.append(elem)
     return lista_locala
 lista1=creare_lista()
